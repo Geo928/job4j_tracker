a779928a77a9962b49a1fb1577bfde5ef958d0b7
Георгий <gkarkazidi@gmail.com>
2022-01-01 15:03:13 +0300

    Взаимодействие объектов [#166087 #245793]

--- src/main/java/ru/job4j/oop/Ball.java
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -2,10 +2,10 @@ package ru.job4j.oop;
 
 public class Ball {
     public void tryRun(boolean condition) {
-    if (condition == false) {
-        System.out.println("the ball escaped");
-    } else {
+    if (condition) {
         System.out.println("the ball is eaten");
+    } else {
+        System.out.println("the ball escaped");
     }
     }
 }
