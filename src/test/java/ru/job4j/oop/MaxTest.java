32ac64d569ac27280ce172fa675aefa8f8298c5c
Георгий <gkarkazidi@gmail.com>
2022-01-02 17:21:04 +0300

    Перегрузить метод max для трех чисел [#122644]

---
+++ src/test/java/ru/job4j/oop/MaxTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.oop;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MaxTest {
+    @Test
+    public void whenMax10() {
+        int expected = 10;
+        int result = Max.max(1, 10);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenMax13() {
+        int expected = 13;
+        int result = Max.max(13, 10, 12);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenMax17() {
+        int expected = 17;
+        int result = Max.max(10, 17, 12, 11);
+        Assert.assertEquals(expected, result);
+    }
+}
