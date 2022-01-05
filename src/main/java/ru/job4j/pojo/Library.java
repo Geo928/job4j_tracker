72954da8adcfe6d4e24364f9ff6d380b2321357c
Георгий <gkarkazidi@gmail.com>
2022-01-05 20:03:26 +0300

    Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,36 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+     Book first = new Book("Clean code", 150);
+     Book second = new Book("Second book", 120);
+     Book third = new Book("Third book", 90);
+     Book fourth = new Book("Fourth book", 200);
+     Book[] book = new Book[4];
+     book[0] = first;
+     book[1] = second;
+     book[2] = third;
+     book[3] = fourth;
+        for (int i = 0; i < book.length; i++) {
+            Book result = book[i];
+            System.out.println(result.getName() + " - " + result.getNumbers());
+        }
+        Book time = book[0];
+        book[0] = book[3];
+        book[3] = time;
+        System.out.println();
+        System.out.println("Replace first and fourth");
+        for (int i = 0; i < book.length; i++) {
+            Book result = book[i];
+            System.out.println(result.getName() + " - " + result.getNumbers());
+        }
+        System.out.println();
+        System.out.println("Only Clean code");
+        for (int i = 0; i < book.length; i++) {
+            Book result = book[i];
+            if (result.getName().equals("Clean code")) {
+                System.out.println(result.getName() + " - " + result.getNumbers());
+            }
+        }
+    }
+}
