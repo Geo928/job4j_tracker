b8bbee5ac3f9295713f824be3075ace73c1a4706
Георгий <gkarkazidi@gmail.com>
2022-01-15 21:12:58 +0300

    111

---
+++ src/main/java/ru/job4j/poly/FileStore.java
@@ -0,0 +1,13 @@
+package ru.job4j.poly;
+
+//public class FileStore implements Store {
+   /* @Override
+    public void save(String value) {
+        super.save(value);
+    }
+
+    @Override
+    public String[] load() {
+        return super.load();
+    }*/
+//}
--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -10,7 +10,6 @@ public class Item {
     private LocalDateTime created = LocalDateTime.now();
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
-
     public Item() {
     }
 
@@ -64,10 +63,10 @@ public class Item {
 
     @Override
     public String toString() {
-        return "Item{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", created=" + created.format(FORMATTER) +
-                '}';
+        return "Item{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", created=" + created.format(FORMATTER)
+                + '}';
     }
 }
