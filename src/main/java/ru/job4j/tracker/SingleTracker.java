5f882763f0ca50c878e682da50cb9dfd8ccb5c2e
Георгий <gkarkazidi@gmail.com>
2022-01-19 11:42:32 +0300

    Singleton [#94606 #252341]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 public final class SingleTracker {
+    private static SingleTracker instance = null;
     private Tracker tracker = new Tracker();
 
     private SingleTracker() {
@@ -28,5 +29,12 @@ public final class SingleTracker {
 
     public boolean delete(int id) {
         return tracker.delete(id);
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
     }
 }
