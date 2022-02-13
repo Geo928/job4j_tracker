0192f5adac16307e24324fb988d695cb0ad79ab5
Георгий <gkarkazidi@gmail.com>
2022-02-13 11:40:45 +0300

    4. Сортировка номера [#221284 #263783]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -5,14 +5,15 @@ import java.util.Comparator;
 public class LexSort implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int result = 0;
-        String[] first = left.split(". Task.");
-        String[] second = right.split(". Task.");
-        for (int i = 0; i < Math.max(first.length, second.length); i++) {
-            int one = Integer.parseInt(first[i]);
-            int two = Integer.parseInt(second[i]);
-            result = Integer.compare(one, two);
-        }
-        return result;
+        String first = left.split("\\.")[0];
+        String second = right.split("\\.")[0];
+        return Integer.compare(Integer.parseInt(first), Integer.parseInt(second));
+    }
+
+    public static void main(String[] args) {
+        LexSort lex = new LexSort();
+        int rsl = lex.compare("1. Task.", "2. task. 3.  5. 5.");
+        System.out.println(rsl);
     }
 }
+
